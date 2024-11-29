3d70d0acdd0496b4b75df113a60e0441f59ee501
ACD-MD-TEAM <acdmdteam@gmail.com>
2024-11-29 20:24:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "SYtwAYSB#vLtap-Aeo2vzCLqqjBmhb2grJm-Qqd_f6IgHMnrTc9A",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94726645160,94768844616,+94701219309",
+SUDO_NB: process.env.SUDO_NB || "94742398003",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
