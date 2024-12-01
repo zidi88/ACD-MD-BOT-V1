dedf5dda82cb46900c816eaf19e3548fa66a6130
ACD-MD-TEAM <acdmdteam@gmail.com>
2024-12-01 11:43:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SYtwAYSB#vLtap-Aeo2vzCLqqjBmhb2grJm-Qqd_f6IgHMnrTc9A",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "add_your_session_id",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "94742398003",
